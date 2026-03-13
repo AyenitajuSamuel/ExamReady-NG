79b3b01dd176ff674d0db7acd427bd21742f8b6a
Samuel <samuelayenitaju@gmail.com>
2026-03-14 00:44:43 +0100

    register-models-in-admin

--- questions/admin.py
+++ questions/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Question, Subject, Choice 
+from .models import Question, Subject, Choice, Topic, Explanation
 
 admin.site.register(Question)
 admin.site.register(Subject)
 admin.site.register(Choice)
+admin.site.register(Topic)
+admin.site.register(Explanation)
